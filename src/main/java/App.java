3caab9092a6492390706777a9625bf3b95cde591
Arthur <arthur.250402@gmail.com>
2021-10-24 16:32:25 -0300

    Revert "First bot login"
    This reverts commit a6c59962e6eab5e269bdf0f24704dc2943380981.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,9 @@
-import javax.security.auth.login.LoginException;
-
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-
-import sensitive.Token;
-
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) throws LoginException {
-        Token token = new Token();
-
-        JDA jda = JDABuilder.createDefault(token.getToken()).build();
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
