df9c58d007d8127aff4e1c0c7fba30ae80842b7b
Arthur <arthur.250402@gmail.com>
2021-10-24 16:42:23 -0300

    First Login

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,15 @@
-/**
- * Hello world!
- *
- */
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import sensitive.Token;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws LoginException {
+        Token token = new Token();
+
+        JDA jda = JDABuilder.createDefault(token.getToken()).build();
+
     }
 }
